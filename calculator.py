99c23a16058387aa418a3fc1e280cacd9709a19f
abbiejonnes <abigaelmwangi534@gmail.com>
2026-07-27 13:29:27 +0300

    fix:update calculator.py file by adding the logical operators

---
+++ calculator.py
@@ -0,0 +1,9 @@
+num1= int(input("Enter num1:"))
+num2= int(input("Enter num2:"))
+num3= int(input("Enter num3:"))
+addition= num1 + num2 + num3
+subtraction= num1 - num3
+division= num2 / num1
+print("addition:", addition)
+print("subtraction:", subtraction)
+print("division:", division)
